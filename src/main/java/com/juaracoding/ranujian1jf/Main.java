ec4709ea15668b6c92d7bf3eabb03d998482a4ab
raisya20 <raisyaa95@gmail.com>
2024-10-08 15:53:09 +0700

    Soal 1: Variabel

---
+++ src/main/java/com/juaracoding/ranujian1jf/Main.java
@@ -0,0 +1,7 @@
+package com.juaracoding.ranujian1jf;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/juaracoding/ranujian1jf/ujian1/Soal1.java
@@ -0,0 +1,14 @@
+package com.juaracoding.ranujian1jf.ujian1;
+
+public class Soal1 {
+    public static void main (String args[]) {
+        String movie = "Avengers: Endgame";
+        int price = 50000;
+        int amount = 3;
+
+        System.out.println("Nama Film: "+movie);
+        System.out.println("Harga Tiket: "+price);
+        System.out.println("Jumlah Tiket: "+amount);
+
+    }
+}
